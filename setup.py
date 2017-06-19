464a29771905639f8385b98d0a1b0334eeeeb949
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2017-06-19 12:46:21 -0700

    updated setup.py and requirements_dev

--- setup.py
+++ setup.py
@@ -12,10 +12,8 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'calmjs',
     'jinja2',
     'pyramid_jinja2',
-    'pyramid_mako',
 ]
 
 setup_requirements = [
@@ -27,12 +25,6 @@ test_requirements = [
     'pytest',
     # TODO: put package test requirements here
 ]
-
-extras_calmjs = {
-    'node_modules': {
-        'pug': '/.bin/pug',
-    }
-}
 
 setup(
     name='ppug',
@@ -64,5 +56,4 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     setup_requires=setup_requirements,
-    extras_calmjs=extras_calmjs,
 )
