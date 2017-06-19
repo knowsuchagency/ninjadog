0adb4999a4e50a95ab966142eb582a4893fba969
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2017-06-19 12:37:18 -0700

    reverted to old jinja2 extension

--- ppug/ext/pyramid/__init__.py
+++ ppug/ext/pyramid/__init__.py
@@ -1,16 +1,6 @@
 from jinja2.ext import Extension
-from pyramid_jinja2 import _caller_package
-
 from pathlib import Path
-
 from ppug import render
-from ppug import render as preprocessor
-
-from pyramid_mako import (
-    MakoRendererFactory,
-    parse_options_from_settings,
-    PkgResourceTemplateLookup,
-    )
 
 
 class PugPreprocessor(Extension):
@@ -23,33 +13,7 @@ class PugPreprocessor(Extension):
         return render(source, template_path=Path(filename))
 
 
-class PugRenderer:
-    def __init__(self, info):
-        info.settings['mako.preprocessor'] = preprocessor
-        factory = MakoRendererFactory()
-        self.makoRenderer = factory(info)
-
-    def __call__(self, value, system):
-        return self.makoRenderer(value, system)
-
-
-def add_pug_renderer(config, extension, mako_settings_prefix='mako.'):
-
-    renderer_factory = MakoRendererFactory()
-    config.add_renderer(extension, renderer_factory)
-
-    def register():
-        settings = config.registry.settings
-        settings['{0}preprocessor'.format(mako_settings_prefix)] = preprocessor
-
-        opts = parse_options_from_settings(settings, mako_settings_prefix, config.maybe_dotted)
-        lookup = PkgResourceTemplateLookup(**opts)
-
-        renderer_factory.lookup = lookup
-
-    config.action(('pug-renderer', extension), register)
-
-
 def includeme(config):
-    config.add_directive('add_pug_renderer', add_pug_renderer)
-    config.add_pug_renderer('.pug')
+    config.add_jinja2_renderer('.pug')
+    config.add_jinja2_extension(PugPreprocessor, name='.pug')
+
