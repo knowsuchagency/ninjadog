f489c98706eba6c1c309b5275e9c48702229cf1a
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2017-06-19 12:33:36 -0700

    used add renderer function instead

--- ppug/ext/pyramid/__init__.py
+++ ppug/ext/pyramid/__init__.py
@@ -33,5 +33,23 @@ class PugRenderer:
         return self.makoRenderer(value, system)
 
 
+def add_pug_renderer(config, extension, mako_settings_prefix='mako.'):
+
+    renderer_factory = MakoRendererFactory()
+    config.add_renderer(extension, renderer_factory)
+
+    def register():
+        settings = config.registry.settings
+        settings['{0}preprocessor'.format(mako_settings_prefix)] = preprocessor
+
+        opts = parse_options_from_settings(settings, mako_settings_prefix, config.maybe_dotted)
+        lookup = PkgResourceTemplateLookup(**opts)
+
+        renderer_factory.lookup = lookup
+
+    config.action(('pug-renderer', extension), register)
+
+
 def includeme(config):
-    config.add_renderer('.pug', PugRenderer)
+    config.add_directive('add_pug_renderer', add_pug_renderer)
+    config.add_pug_renderer('.pug')
