56cd0574260ebd766d31fdc46346dd34788dad05
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2017-06-19 11:38:59 -0700

    updated preprocessor

--- ppug/ext/pyramid/__init__.py
+++ ppug/ext/pyramid/__init__.py
@@ -15,22 +15,5 @@ class PugPreprocessor(Extension):
 
 def includeme(config):
     config.add_jinja2_renderer('.pug')
-    config.add_jinja2_extension(PugPreprocessor, name='.pug')
+    config.add_jinja2_extension(PugPreprocessor)
 
-
-if __name__ == '__main__':
-    from jinja2 import Environment
-    from jinja2 import FileSystemLoader
-    from tempfile import TemporaryDirectory, NamedTemporaryFile
-    with TemporaryDirectory() as tempdir:
-        with NamedTemporaryFile(dir=tempdir, suffix='.pug') as fp:
-            string = 'h1 hello {{ name }}'
-            fp.write(string.encode('utf8'))
-            fp.seek(0)
-
-            loader = FileSystemLoader(tempdir)
-            env = Environment(extensions=(PugPreprocessor,),
-                              loader=loader)
-            print(env.list_templates())
-            template = env.get_or_select_template(env.list_templates())
-            print(template.render(name='Stephan Fitzpatrick'))
