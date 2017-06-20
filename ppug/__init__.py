c16b04a8fa86eb1da707e10afc6e278cb5bdee08
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2017-06-19 21:57:26 -0700

    Bump version: 0.1.1 → 0.2.0

--- ppug/__init__.py
+++ ppug/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Stephan Fitzpatrick"""
 __email__ = 'knowsuchagency@gmail.com'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 from .ppug import render
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='ppug',
-    version='0.1.1',
+    version='0.2.0',
     description="Pug template support in Python",
     long_description=readme + '\n\n' + history,
     author="Stephan Fitzpatrick",
