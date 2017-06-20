79b20ee76c78a546721aeec492dd47f4520faf69
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2017-06-19 21:57:07 -0700

    ignored files

--- build/lib/ppug/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for ppug."""
-
-__author__ = """Stephan Fitzpatrick"""
-__email__ = 'knowsuchagency@gmail.com'
-__version__ = '0.1.1'
-
-from .ppug import render
