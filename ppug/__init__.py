ff33d9a2351eedf9f441c112551e2e483567249e
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2017-06-21 01:03:01 -0700

    imports

--- ppug/__init__.py
+++ ppug/__init__.py
@@ -6,6 +6,6 @@ __author__ = """Stephan Fitzpatrick"""
 __email__ = 'knowsuchagency@gmail.com'
 __version__ = '0.4.1'
 
-from ppug.ppug import render
-from ppug.ext.jinja2 import jinja2_renderer
 from ppug import ext
+from ppug.ext.jinja2 import jinja2_renderer
+from ppug.ppug import render
