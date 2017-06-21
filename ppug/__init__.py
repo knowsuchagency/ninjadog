f78180b79d71a9e32d0c8dcdd8a9f69f8d1bf808
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2017-06-21 01:18:57 -0700

    more import stuff

--- ppug/__init__.py
+++ ppug/__init__.py
@@ -6,6 +6,5 @@ __author__ = """Stephan Fitzpatrick"""
 __email__ = 'knowsuchagency@gmail.com'
 __version__ = '0.4.1'
 
-from ppug import ext
 from ppug.ppug import render
 from ppug.ext.jinja2 import jinja2_renderer
