d5b6cf8a5b6adda8bff097a34d808325641c2b3d
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2017-06-21 02:40:01 -0700

    Bump version: 0.4.3 → 0.4.4

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='ppug',
-    version='0.4.3',
+    version='0.4.4',
     description="Pug template support in Python",
     long_description=readme + '\n\n' + history,
     author="Stephan Fitzpatrick",
