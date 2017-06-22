d0d59f0006d967cc65a2f31b62e6d366f8bcfcec
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2017-06-22 00:44:21 -0700

    bugfix

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='ninjadog',
-    version='version=0.1.0',
+    version='0.1.0',
     description="Pug template support in Python",
     long_description=readme,
     author="Stephan Fitzpatrick",
