32936ddf0c83eaac1028c7f60248b06be9abe311
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2017-06-22 18:57:22 -0700

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='ninjadog',
-    version='0.1.2',
+    version='0.1.3',
     description="Pug template support in Python",
     long_description=readme,
     author="Stephan Fitzpatrick",
