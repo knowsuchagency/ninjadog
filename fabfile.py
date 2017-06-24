3c79880d2e5667d56647636eb59678e29271a963
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2017-06-24 00:24:14 -0700

    added fabric to requirements. beginning to transition makefile to fabric

---
+++ fabfile.py
@@ -0,0 +1,10 @@
+from fabric.api import *
+
+def clean_build():
+    """Remove build artifacts"""
+    local('rm -fr build/')
+    local('rm -fr dist/')
+    local('rm -rf .eggs/')
+    local("find . -name '*.egg-info' -exec rm -fr {} +")
+    local("find . -name '*.egg' -exec rm -f {} +")
+
